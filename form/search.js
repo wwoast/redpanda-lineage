c566f6cdaad215080606fcef365a3d510b4e011b
Justin Fairchild <boil.afraid@gmail.com>
2018-06-16 14:59:03 -0700

    js db loads into the page hackily

--- form/search.js
+++ form/search.js
@@ -3,5 +3,33 @@
     Built with love and Dagoba.js :)  
 */
 
-var Panda = {};
-var db = JSON.parse(document.getElementById("panda_db"));
+
+var Pandas = {};   // Namespace
+
+Pandas.P = {};     // Prototype
+
+Pandas.init = function() {
+  /* Create a Pandas search object, and do the initial database import.
+  
+  XMLHttpRequest the JSON panda data once all basic scripts have loaded.
+  Then return the big chunk of data, along with our query functions.
+  */ 
+  var db_url = "https://wwoast.github.io/redpanda-lineage/export/redpanda.json";
+ 
+  var pandas = Object.create(Pandas.P); 
+  var request = new XMLHttpRequest();
+  request.open('GET', db_url);
+  request.responseType = 'json';
+  request.send();
+  request.onload = function() {
+    pandas.db = request.response;
+  }
+
+  return pandas;
+}
+
+/*
+Pandas.P.test_output = function(db) {
+  return JSON.stringify(db);
+}
+*/
