fce7d86b67a31a608ebab127af5412c2d0c69fcb
Justin Fairchild <boil.afraid@gmail.com>
2018-07-12 08:35:21 -0700

    mobile font scaling and metatags

--- search/js/show.js
+++ search/js/show.js
@@ -1,4 +1,24 @@
 /*
+    Mobile meta-tag support for various phone/tablet font scales
+*/
+(function() {
+  if ( navigator.platform === "iPad" ) {
+          var scale = 1.2;
+          document.write('<meta name="viewport" content="width=device-width; initial-scale='+scale+'; minimum-scale='+scale+'; maximum-scale='+scale+'; user-scalable=0;" />');
+
+  } else if ( navigator.platform === "iPhone" ) {
+          var scale = 1.0;
+          document.write('<meta name="viewport" content="width=device-width; initial-scale='+scale+'; minimum-scale='+scale+'; maximum-scale='+scale+'; user-scalable=0;" />');
+
+  } else if ( navigator.userAgent.indexOf("Android") != -1 ) {
+          var scale = 1.2;
+          document.write('<meta name="viewport" content="width=device-width; initial-scale-'+scale+'; minimum-scale='+scale+'; maximum-scale='+scale+'; user-scalable=0; target-densitydpi="device-dpi"; />');
+  } else {
+          return;
+  }
+})(); 
+
+/*
     Global objects usable by forms, and things that operate as the page loads
 */
 var P;   // Pandas
