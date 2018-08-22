eed9afe2737c48a912c3c88d9845b9e92fcfc058
Justin Fairchild <boil.afraid@gmail.com>
2018-08-21 23:00:36 -0700

    half sibling icon only appears when it's certain they are a half-sibling

--- search/js/show.js
+++ search/js/show.js
@@ -747,7 +747,8 @@ Show.displayPandaSiblings = function(info) {
     var options = ["child_icon", "live_icon"];
     var test_mom = Pandas.searchPandaMom(animal["_id"]);
     var test_dad = Pandas.searchPandaDad(animal["_id"]);
-    if (!((test_mom == info.mom) && (test_dad == info.dad))) {
+    if (!((test_mom == info.mom) && (test_dad == info.dad)) &&
+         ((test_mom != undefined) && (test_dad != undefined))) {
       options.push("half_icon");
     }
     var siblings_link = Show.animalLink(animal, animal[info.get_name], 
