b604ab2a3865f709e558e0a3780a5fb3c37c7d0b
Justin Fairchild <boil.afraid@gmail.com>
2018-09-25 09:12:06 -0700

    zoo photo fixes check correct defaults now

--- search/js/show.js
+++ search/js/show.js
@@ -1262,7 +1262,7 @@ Show.displayZooDetails = function(info) {
   details.appendChild(zoo_page);
   // Photo details are optional for zoos, so don't show the
   // photo link if there's no photo included in the dataset
-  if (info.photo != undefined) {
+  if (info.photo != Pandas.def.zoo["photo"]) {
     var photo_page = document.createElement('p');
     var photo_link = document.createElement('a');
     photo_link.href = info.photo_link;
