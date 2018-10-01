b43d46731651056408d9f48cff76b2bf2ca987fb
Justin Fairchild <boil.afraid@gmail.com>
2018-09-30 20:59:13 -0700

    meta tag font scaling fixes for mobile, should fix android

--- search/js/show.js
+++ search/js/show.js
@@ -3,18 +3,16 @@
 */
 (function() {
   if ( navigator.platform === "iPad" ) {
-          var scale = 1.2;
-          document.write('<meta name="viewport" content="width=device-width; initial-scale='+scale+'; minimum-scale='+scale+'; maximum-scale='+scale+'; user-scalable=0;" />');
-
+    var scale = 1.2;
+    document.write('<meta name="viewport" content="width=device-width, initial-scale='+scale+', minimum-scale='+scale+', maximum-scale='+scale+', user-scalable=0" />');
   } else if ( navigator.platform === "iPhone" ) {
-          var scale = 1.0;
-          document.write('<meta name="viewport" content="width=device-width; initial-scale='+scale+'; minimum-scale='+scale+'; maximum-scale='+scale+'; user-scalable=0;" />');
-
+    var scale = 1.0;
+    document.write('<meta name="viewport" content="width=device-width, initial-scale='+scale+', minimum-scale='+scale+', maximum-scale='+scale+', user-scalable=0" />');
   } else if ( navigator.userAgent.indexOf("Android") != -1 ) {
-          var scale = 1.2;
-          document.write('<meta name="viewport" content="width=device-width; initial-scale-'+scale+'; minimum-scale='+scale+'; maximum-scale='+scale+'; user-scalable=0; target-densitydpi="device-dpi"; />');
+    var scale = 1.0;
+    document.write('<meta name="viewport" content="width=device-width, initial-scale-'+scale+', minimum-scale='+scale+', maximum-scale='+scale+', user-scalable=0, target-densitydpi="device-dpi" />');
   } else {
-          return;
+    return;
   }
 })();
 
