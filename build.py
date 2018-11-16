b0e9d529514575d613439f74816ffb11c721119e
Justin Fairchild <justin@boil.afraid.org>
2018-11-16 14:08:31 -0800

    shared values for python tools

--- build.py
+++ build.py
@@ -9,27 +9,7 @@ import json
 import os
 import sys
 
-PANDA_PATH = "./pandas"
-OUTPUT_PATH = "./export/redpanda.json"
-ZOO_PATH = "./zoos" 
-
-class DateConsistencyError(ValueError):
-    pass
-
-class DateFormatError(ValueError):
-    pass
-
-class GenderFormatError(ValueError):
-    pass
-
-class IdError(KeyError):
-    pass
-
-class LinkError(IndexError):
-    pass
-
-class NameFormatError(ValueError):
-    pass
+from shared import *
 
 class RedPandaGraph:
     """Class with the redpanda database and format/consistency checks.
