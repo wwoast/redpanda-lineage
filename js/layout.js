d560b3fe84803a8aa3efe1996dc39dc423a835f8
Justin Fairchild <boil.afraid@gmail.com>
2019-01-14 23:14:59 -0800

    variable-name function calling strategy for manual-switchboarding layout features

--- js/layout.js
+++ js/layout.js
@@ -25,11 +25,11 @@ Layout.init = function(family, info, parents, litter, siblings, children) {
     // Make sure checks can see this object's counts
     layout.checks.num = layout.num;
     // Set up the divs themselves
-    layout.family = family;
-    layout.parents = parents;
-    layout.litter = litter;
-    layout.siblings = siblings;
-    layout.children = children;
+    layout.arrangement.family = family;
+    layout.arrangement.parents = parents;
+    layout.arrangement.litter = litter;
+    layout.arrangement.siblings = siblings;
+    layout.arrangement.children = children;
     return layout;
 }
 
@@ -39,15 +39,222 @@ Layout.L.num.litter = 0;
 Layout.L.num.siblings = 0;
 Layout.L.num.children = 0;
 
-Layout.L.family = undefined;   /* Output of this layout tool */
-Layout.L.parents = undefined;
-Layout.L.litter = undefined;
-Layout.L.siblings = undefined;
-Layout.L.children = undefined;
+/* Given either a value or a range of values, validate that the available animals
+   in that list matches the count given of them. For simplicity, assume inclusive */
+Layout.L.count = function(p=0, l=0, s=0, c=0) {
+  // Create ranges out of inputs
+  var range = {
+    "parents": p,
+    "litter": l,
+    "siblings": s,
+    "children": c
+  };
+  // Standardize on ranges
+  for (key in range) {
+    value = range[key];
+    if (isFinite(arg)) {
+      range[key] = [value, value]
+    } else if (Array.isArray(value)) {
+      if (isFinite(value[0]) && isFinite(value[1])) {
+        range[key] = [value[0], value[1]];
+      } else {
+        range[key] = [0, 0];
+      }
+    }
+  }
+  // Confirm the values of each input
+  return ((this.between(this.num.parents, range["parents"][0], range["parents"][1])) &&
+          (this.between(this.num.litter, range["litter"][0], range["litter"][1])) &&
+          (this.between(this.num.siblings, range["siblings"][0], range["siblings"][1])) &&
+          (this.between(this.num.children, range["children"][0], range["children"][1])));
+}
 
-Layout.L.arrangements = {};
 // TOWRITE: define arrangements here, instead of nesting them in the generate.layout.
-// Include column swaps, tr swaps, and things in these functions.
+// Include column swaps, tr swaps, and things in these functions. Since the
+// TODO!! L.div functions need to just be class methods/utilties.
+Layout.L.arrangement = {};
+Layout.L.arrangement.default = ["parents", "litter", "siblings", "children"];
+Layout.L.arrangement.order = ["parents", "litter", "siblings", "children"];
+Layout.L.arrangement.family = undefined;   /* Output of this layout tool */
+Layout.L.arrangement.parents = undefined;
+Layout.L.arrangement.litter = undefined;
+Layout.L.arrangement.siblings = undefined;
+Layout.L.arrangement.children = undefined;
+
+// TODO: this may be the basis of a basic layout which takes arguments:
+// flattenTop, multiColumn, others.
+// Take all inputs and display as straight columns.
+Layout.L.arrangement.columns = function() {
+  return;
+}
+
+// Take the longest column and make into a multicolumn list.
+// Any other columns that exist should be displayed as straight columns.
+// Mode can be "onlyMobile", "onlyDesktop", or "both"
+Layout.L.arrangement.multiColumn = function(columns, mode="both") {
+  return;
+}
+
+// Flatten a single column, both on mobile and desktop
+Layout.L.arrangement.flatten = function() {
+  return;
+}
+
+// Flatten the first column, but keep the others. 
+// Top flat column is only done on mobile layouts
+Layout.L.arrangement.flattenTop = function() {
+  return;
+}
+
+// Combination of flattenTop and multiColumn.
+// TODO: need desktop or mobile modal flags for BOTH the flattening and the multicolumn mode
+Layout.L.arrangement.flattenTopMultiColumn = function(columns, mode="both") {
+  return;
+}
+
+// In some extraordinary cases on mobile, multiColumn on three-element lists 
+// may look better than the other alternatives. Use this layout to guarantee
+// that short lists still get flowed multicolumn.
+Layout.L.arrangement.shortMultiColumn = function(columns, mode="both") {
+  return;
+}
+
+// When sparse columns stacked up may be nearly as long as a third column,
+// kick the little ones out and let the longer column run. If a fourth column
+// appears, display it underneath the long column.
+Layout.L.arrangement.longRun = function() {
+  return;
+}
+
+// Where the last column displayed in the order must be the longest on mobile
+// for space to be preserved
+Layout.L.arrangement.lastColumnLong = function() {
+  return;
+}
+
+// If the lookup tables find no arrangement, use these default heuristics.
+// -- Two columns, one short and one long? => flattenTopMultiColumn
+// -- Similar columns no deeper than four? => columns
+// -- One long column, two shorts, and long column is 8 or less? => longRun
+// -- One long column, two shorts, and long column is 9 or more => multiColumn
+// -- Two long columns, shorter no less than 5, longest no more than 8? => longRun
+// -- Long column of 9 or more? Take all columns longer than 5 and multiColumn them
+Layout.L.arrangement.default = function() {
+  return;
+}
+
+/* The arrangement switchboard. Set these arrangement names equivalent 
+   to the actual functions that will perform the layout duties.
+   Comments refer to mobile layout considerations. */
+// TODO: remove any switchboard entries that don't need speical behavior!!
+// One list item? Simple column layout is fine.
+Layout.L.arrangement.div1_1_0_0_0 = Layout.L.arrangement.columns;
+// Two list items, horizontal display to save space. 
+Layout.L.arrangement.div2_2_0_0_0 = Layout.L.arrangement.flatten;
+// Two list items, in their own columns
+Layout.L.arrangement.div2_1_1_0_0 = Layout.L.arrangement.columns;
+// Three list items. Two parents and a single third
+Layout.L.arrangement.div3_2_1_0_0 = Layout.L.arrangement.columns;
+// Three list items. One each in three lists
+Layout.L.arrangement.div3_0_1_1_1 = Layout.L.arrangement.columns;
+// Three list items. One column of three items
+Layout.L.arrangement.div3_0_0_3_0 = Layout.L.arrangement.columns;
+// Four list items. Two parents and two in a second column
+Layout.L.arrangement.div4_2_2_0_0 = Layout.L.arrangement.columns;
+// Four list items. Two in one category and singles
+// TODO: possibly long run, but find a way to kick the spacers right
+Layout.L.arrangement.div4_2_1_1_0 = Layout.L.arrangement.flattenTop;
+// Four list items. Three in one category
+Layout.L.arrangement.div4_0_0_3_1 = Layout.L.arrangement.columns;
+// Four list items. All in one category. Longer lists will get multiColumn'ed
+Layout.L.arrangement.div4_0_0_4_0 = Layout.L.arrangement.columns;
+// Five list items. Two parents and three in a second column
+Layout.L.arrangement.div5_2_0_3_0 = Layout.L.arrangement.columns;
+// Five list items. Two parents and a two/one split.
+// TODO: consider flattenTop for this style of layout
+Layout.L.arrangement.div5_2_2_1_0 = Layout.L.arrangement.columns;
+// Five list items. Two parents and three other columns
+Layout.L.arrangement.div5_2_1_1_1 = Layout.L.arrangement.columns;
+// Five list items. Four in one list, and a fifth
+Layout.L.arrangement.div5_0_0_4_1 = Layout.L.arrangement.columns;
+// Five list items. One list, but split multicolumn
+Layout.L.arrangement.div5_0_0_5_0 = Layout.L.arrangement.multiColumn(2);
+// Six list items. Two parents and others in a single column
+Layout.L.arrangement.div6_2_0_4_0 = Layout.L.arrangement.columns;
+// Six list items. Two parents and a short split
+Layout.L.arrangement.div6_2_3_1_0 = Layout.L.arrangement.longRun;
+// Six list items. Two parents and even columns
+Layout.L.arrangement.div6_2_2_2_0 = Layout.L.arrangement.flattenTop;
+// Six list items. Two parents and spread
+Layout.L.arrangement.div6_2_2_1_1 = Layout.L.arrangement.columns;
+// Six list items. Mostly or all in one column
+Layout.L.arrangement.div6_0_0_5_1 = Layout.L.arrangement.multiColumn(2);
+Layout.L.arrangement.div6_0_0_6_0 = Layout.L.arrangement.multiColumn(2);
+// Six list items. All in two columns
+Layout.L.arrangement.div6_0_0_3_3 = Layout.L.arrangement.columns;
+// Seven list items. Parents, and a multicolumn
+Layout.L.arrangement.div7_2_0_5_0 = Layout.L.arrangement.flattenTopMultiColumn(2);
+// Seven list items. Parents, litter, and three siblings
+Layout.L.arrangement.div7_2_2_3_0 = Layout.L.arrangement.longRun;
+// Seven list items. parents, litter, siblings, and children, all straight columns
+Layout.L.arrangement.div7_2_2_2_1 = Layout.L.arrangement.columns;
+// Seven list items. A predominant list with parents.
+// The three item list is last, but we can shift it up a little
+// TODO: might try a flat layout on top, and a longRun lower down
+Layout.L.arrangement.div7_2_1_3_1 = Layout.L.arrangement.longRun;
+// Seven list items. a predominant list with no litter. 
+// Place the one below the parents, and the four kicked right.
+Layout.L.arrangement.div7_2_0_4_1 = Layout.L.arrangement.longRun;
+// Eight list items. Two animals per list type
+Layout.L.arrangement.div8_2_2_2_2 = Layout.L.arrangement.columns;
+// Eight list items. Mostly balanced but a three-and-one on the right
+Layout.L.arrangement.div8_2_2_3_1 = Layout.L.arrangement.longRun;
+// Eight list items, a long column and two shorties
+Layout.L.arrangement.div8_2_2_4_0 = Layout.L.arrangement.longRun;
+Layout.L.arrangement.div8_2_1_5_0 = Layout.L.arrangement.longRun;
+// Eight list items. Do the four column below the one, but kick it up
+Layout.L.arrangement.div8_2_1_4_1 = Layout.L.arrangement.lastColumnLong;
+// Eight list items. Flatten the top and multicolumn the other one
+Layout.L.arrangement.div8_2_0_6_0 = Layout.L.arrangement.flattenTopMultiColumn(2);
+// Nine list items. Mostly balanced
+Layout.L.arrangement.div9_2_2_3_3 = Layout.L.arrangement.columns;
+// Nine list items. Mostly balanced, but kick the last column up slightly
+Layout.L.arrangement.div9_2_1_4_2 = Layout.L.arrangement.lastColumnLong;
+// Nine list items. Two single columns sneaking on the left
+Layout.L.arrangement.div9_2_1_5_1 = Layout.L.arrangement.longRun;
+// Nine list items. A long column goes multiColumn. 
+// On mobile the broader multicolumn lists should shrink down to two columns
+Layout.L.arrangement.div9_2_2_5_0 = Layout.L.arrangement.multiColumn(2);
+Layout.L.arrangement.div9_2_1_6_0 = Layout.L.arrangement.multiColumn(2);
+// Nine list items, but a single column of three looks out of place here.
+Layout.L.arrangement.div9_0_0_6_3 = Layout.L.arrangement.shortMultiColumn(2);
+Layout.L.arrangement.div9_2_0_7_0 = Layout.L.arrangement.flattenTopMultiColumn(2);
+Layout.L.arrangement.div9_0_0_8_1 = Layout.L.arrangement.flattenTopMultiColumn(3);
+Layout.L.arrangement.div9_0_0_9_0 = Layout.L.arrangement.flattenTopMultiColumn(4);
+// Ten list items. Parents and balanced elsewhere
+Layout.L.arrangement.div10_2_0_4_4 = Layout.L.arrangement.flattenTop;
+// Ten list items. Spread out wide
+Layout.L.arrangement.div10_2_2_3_3 = Layout.L.arrangement.columns;
+Layout.L.arrangement.div10_2_1_4_3 = Layout.L.arrangement.lastColumnLong;
+// Ten list items. Flatten the top, and multicolumn the largest one
+Layout.L.arrangement.div10_2_2_5_1 = Layout.L.arrangement.flattenTopMultiColumn(2);
+// Ten list items. Balanced lists and a muticolumn
+Layout.L.arrangement.div10_2_2_6_0 = Layout.L.arrangement.multiColumn(2);
+// Ten list items. Sneak the singles down the left
+Layout.L.arrangement.div10_2_1_6_1 = Layout.L.arrangement.longRun;
+// Ten list items. Too long to be a long run.
+Layout.L.arrangement.div10_2_0_7_1 = Layout.L.arrangement.multiColumn(2);
+// Ten list items. No parents layouts, even stevens. Spread out on desktop
+Layout.L.arrangement.div10_0_0_5_5 = Layout.L.arrangement.multiColumn(2, 'onlyDesktop');
+// Ten list items. On mobile this looks fine as two columns.
+// On desktop the multicolumn div needs to be split out.
+Layout.L.arrangement.div10_0_0_6_4 = Layout.L.arrangement.multiColumn(2, 'onlyDesktop');
+// Ten list items. Seven-long columns are too much.
+Layout.L.arrangement.div10_0_0_7_3 = Layout.L.arrangement.shortMultiColumn(2);
+
+
+
+// Ten list items. Two columns of five should both be multiColumn'ed
 
 Layout.L.checks = {};
 // If children and siblings within one animal difference of each other in size,
@@ -236,20 +443,49 @@ Layout.L.div.swapColumn = function(target, destination, destination_cnt) {
   target.parentNode.insertBefore(divBreak, destination.nextSibling);
 }
 
-/* For a single list, run through the relevant layout rules */
-Layout.L.layoutList = function(list_div, list_name, list_count) {
-  if ((list_div == undefined) || (list_count == 0)) {
-    return;
+/* Create all permutations of an input string. This is used to determine the
+   arrangements of parents and children in the layout function */
+Layout.L.permutations = function(input) {
+  var results = [];
+  if (input.length == 1) {
+    return input;
   }
-  var default_order = ["parents", "litter", "siblings", "children"];
-  // Just this column, and it's short? Make it flat on desktop and mobile
-  if (this.checks.onlyThisListHasMembers(list_name) && list_count == 2) {
-    list_div = this.div.flatten(list_div, onlyMobile=false);
+  for (var i = 0; i < input.length; i++) {
+    var firstVal = input[i];
+    var valsLeft = input.slice(0, i).concat(input.slice(i + 1));
+    var innerPermutations = this.permutations(valsLeft);
+    for (var j = 0; j < innerPermutations.length; j++) {
+      results.push([firstVal].concat(innerPermutations[j]));
+    }
   }
-  // TODO: If a single short column after a long column, swap them.
-  // TODO: If a single other column exists with x > 6 items, flatten on mobile
-  // TODO: If three columns, and one is long, swap non-parent column with the longer one
+  return results;
 }
+
+/* Layout manager. Looks at counts of each element, and gives an arrangement */
+Layout.L.layout = function() {
+  // Given the counts and sum, create a function name to call as an index
+  var sum = (this.num.parents + this.num.litter + this.num.siblings + this.num.children).toString();
+  var orders = this.permutations(this.arrangement.order);
+  // Find the name of an arrangement function based on the lits counts.
+  // divn_parentcnt_littercnt_siblingcnt_childcnt => example: div6_2_1_3_0
+  var arrange_id = undefined;
+  for (order of orders) {
+    this.arrangement.order = order;
+    var test = "div" + sum + "_" + order.map(list => this.num[list]).join("_");
+    if (this.arrangement.indexOf(test) != -1) {
+      arrange_id = test;
+      break;
+    }
+  }
+  // Call an arrangement function if it exists
+  if (arrange_id != undefined) {
+    this.arrangement[arrange_id]();
+  } else {
+    // TODO: if no arrangement function, what to do?
+    // Fall back to a broader algorithm
+  }
+}
+
 
 /* The layout generator basically prods all the possible arrangements of 
    parents/litter/siblings/children, and based on hand-layout-optimizing, chooses
