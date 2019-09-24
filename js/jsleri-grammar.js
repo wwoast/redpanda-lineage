fee5a3044efe581dc200c29365f31655a7268502
Justin Fairchild <boil.afraid@gmail.com>
2019-09-24 09:28:59 -0700

    basic grammar: born year or died year

---
+++ js/jsleri-grammar.js
@@ -0,0 +1,34 @@
+'use strict';
+
+(function (
+            Regex,
+            Sequence,
+            Keyword,
+            THIS,
+            Prio,
+            Grammar
+        ) {
+    var k_born = Keyword('born');
+    var k_died = Keyword('died');
+    var r_year = Regex('(?:[12]{1}[0-9]{3})');
+    var s_born_year = Sequence(k_born, r_year);
+    var s_died_year = Sequence(k_died, r_year);
+    var START = Prio(
+        r_year,
+        k_born,
+        k_died,
+        s_born_year,
+        s_died_year,
+        Sequence('(', THIS, ')'),
+        Sequence(THIS, Keyword('or'), THIS),
+        Sequence(THIS, Keyword('and'), THIS)
+    );
+    window.Grammar = Grammar(START);
+})(
+    window.jsleri.Regex,
+    window.jsleri.Sequence,
+    window.jsleri.Keyword,
+    window.jsleri.THIS,
+    window.jsleri.Prio,
+    window.jsleri.Grammar
+);
