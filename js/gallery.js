ba2fbc450b3b654e95746358f9a4f984dbdf5401
Justin Fairchild <boil.afraid@gmail.com>
2020-06-02 09:26:26 -0700

    make it 5 photo limit for birthday bears

--- js/gallery.js
+++ js/gallery.js
@@ -359,8 +359,8 @@ Gallery.condenseDogEar = function(nav) {
 // Display a birthday header above the photos and credit messages below each one.
 Gallery.birthdayPhotoCredits = function(language, photo_count=3) {
   var birthday_div = document.createElement('div');
-  // Pandas must be alive, and have at least 10 photos
-  var birthday_animals = Pandas.searchBirthday(true, 10);
+  // Pandas must be alive, and have at least 5 photos
+  var birthday_animals = Pandas.searchBirthday(true, 5);
   for (let animal of birthday_animals) {
     var info = Show.acquirePandaInfo(animal, language);
     var years_old = Pandas.ageYears(animal);
