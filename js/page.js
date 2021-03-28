ef6a6510b469836ea19c7ebe265996d064e44323
wwoast <boil.afraid@gmail.com>
2021-03-28 11:58:25 -0700

    photobump, maruko :heart: removed dig feature

--- js/page.js
+++ js/page.js
@@ -335,11 +335,6 @@ Page.home.special_galleries = function() {
       "message": Message.lunch_time,
       "photo_count": 3,
       "taglist": ["bamboo", "bite", "portrait"],
-    },
-    {
-      "message": Message.shovel_pandas,
-      "photo_count": 3,
-      "taglist": ["dig"]
     }
   ];
   var choice = Query.env.paging.seed % special_galleries.length;
