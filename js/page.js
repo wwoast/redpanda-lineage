07b79124b8ed0a1582e45dc279a2983d7d597c81
wwoast <boil.afraid@gmail.com>
2021-07-23 07:23:10 -0700

    goodbye hiko

--- js/page.js
+++ js/page.js
@@ -292,11 +292,12 @@ Page.home.render = function() {
     var new_content = document.createElement('div');
     new_content.className = "results birthdayPandas";
     new_content.id = "contentFrame";
+    // Kin Gin special
+    // var kg = Gallery.memorialPhotoCreditsGroup(L.display, "media.7.gin-kin", ["22", "17"], 3);
+    // new_content.appendChild(kg);
     // Current memorials
-    var kg = Gallery.memorialPhotoCreditsGroup(L.display, "media.7.gin-kin", ["22", "17"], 3);
-    new_content.appendChild(kg);
-    // var departed = Gallery.memorialPhotoCredits(L.display, ["60"], 5, Message.memorial);
-    // new_content.appendChild(departed);
+    var departed = Gallery.memorialPhotoCredits(L.display, ["70"], 5, Message.memorial);
+    new_content.appendChild(departed);
     // Please remember these pandas
     // var memorial = Gallery.memorialPhotoCredits(L.display, ["11"], 5, Message.missing_you);
     // new_content.appendChild(memorial);
