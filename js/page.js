3488afccc57e5cab009a26e2d8139ee96d48450c
wwoast <boil.afraid@gmail.com>
2021-11-10 07:01:19 -0800

    halloween is done

--- js/page.js
+++ js/page.js
@@ -296,8 +296,8 @@ Page.home.render = function() {
     // var kg = Gallery.memorialPhotoCreditsGroup(L.display, "media.7.gin-kin", ["22", "17"], 3);
     // new_content.appendChild(kg);
     // Halloween
-    var halloween = Gallery.special.pumpkin(L.display, 3);
-    new_content.appendChild(halloween);
+    // var halloween = Gallery.special.pumpkin(L.display, 3);
+    // new_content.appendChild(halloween);
     // Current memorials
     var departed = Gallery.memorialPhotoCredits(L.display, ["313"], 3, Message.memorial);
     new_content.appendChild(departed);
