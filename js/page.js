d9735cd4af5512a9a7c5b50384f0814f0f164dd2
wwoast <boil.afraid@gmail.com>
2023-08-01 18:05:14 -0700

    kovu passed

--- js/page.js
+++ js/page.js
@@ -299,7 +299,7 @@ Page.home.render = function() {
     // var kingin = Gallery.memorialPhotoCreditsGroup(L.display, "media.7.gin-kin", ["22", "17"], 3);
     // new_content.appendChild(kingin);
     // Current memorials
-    var memorial_ids = ["201", "1287"];
+    var memorial_ids = ["422", "201", "1287"];
     var departed = Gallery.memorialPhotoCredits(L.display, memorial_ids, 3, Message.memorial);
     new_content.appendChild(departed);
     // Please remember these pandas
