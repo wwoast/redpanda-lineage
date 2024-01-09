7a9cdf3705252e265ede8c850b83becea4ba1bed
Justin Cassidy <justin@boil.afraid.org>
2024-01-09 12:53:00 -0800

    hacky git crap with commits

--- submissions.py
+++ submissions.py
@@ -21,15 +21,17 @@
 #
 
 from shared import ProperlyDelimitedConfigParser
-from collections import OrderedDict
 from PIL import Image
 from datetime import datetime
+import git
 import json
 import os
 import subprocess
 import sys
 
 RESIZE = 400   # pixels
+PANDA_INDEX = {}
+ZOO_INDEX = {}
 
 def convert_json_to_configparser(metadata_path, metadata_file):
     currentTime = datetime.now()
@@ -141,6 +143,16 @@ def copy_review_data_from_submissions_server(config):
         sys.exit(result)
     return processing_folder
 
+def count_until_next_photo(config, section):
+    index = 1
+    while True:
+      try:
+          config.get(section, 'photo.{index}'.format(index=index))
+          index = index + 1
+          continue
+      except:
+          return index
+
 def display_images(photo_paths):
     """Use xli to open photos for a particular metadata file"""
     xli_command = ["xli"]
@@ -163,6 +175,37 @@ def get_image_locators(contribution_path, metadata_path, metadata_file):
         photo_paths.append(os.path.join(contribution_path, locator))
     return photo_paths
 
+def git_new_branch(repo):
+    """Creates a new branch: submissions-YYYY-MM-DD-HH:mm"""
+    currentTime = datetime.now()
+    branchName = 'submissions-{year}-{month}-{day}-{hour}:{minute}'.format(
+        year=currentTime.year,
+        month=currentTime.month,
+        day=currentTime.day,
+        hour=currentTime.hour,
+        minute=currentTime.minute
+    )
+    # Open the Git repo and set to a new branch
+    newBranch = repo.create_head(branchName)
+    repo.head.reference = newBranch
+
+def index_zoos_and_animals():
+    """Index each four-digit ID by folder path"""
+    for _, country in enumerate(os.listdir("./pandas")):
+        country_path = os.path.join("./pandas", country)
+        for _, zoo in enumerate(os.listdir(country_path)):
+            zoo_path = os.path.join(country_path, zoo)
+            for _, panda in enumerate(os.listdir(zoo_path)):
+                panda_path = os.path.join(zoo_path, panda)
+                id = panda_path.split("_")[0]
+                PANDA_INDEX[id] = panda_path
+    for _, country in enumerate(os.listdir("./zoos")):
+        country_path = os.path.join("./zoos", country)
+        for _, zoo in enumerate(os.listdir(country_path)):
+            zoo_path = os.path.join(country_path, zoo)
+            id = zoo_path.split("_")[0]
+            ZOO_INDEX[id] = zoo_path
+
 def iterate_through_contributions(processing_path):
     """Look at pandas, zoos, and then individual photos in each contribution"""
     for _, subfolder in enumerate(sorted(os.listdir(processing_path))):
@@ -174,39 +217,123 @@ def iterate_through_contributions(processing_path):
         for _, subfile in enumerate(sorted(os.listdir(contribution_path))):
             contribution_file = os.path.join(contribution_path, subfile)
             if ".panda.json" in contribution_file:
-                result = process_entity(contribution_path, contribution_file)
+                result = process_entity(contribution_path, contribution_file, "panda")
                 processed.append(contribution_file)
                 if result["status"] == "keep":
                   results.append(result)
         for _, subfile in enumerate(sorted(os.listdir(contribution_path))):
             contribution_file = os.path.join(contribution_path, subfile)
             if ".zoo.json" in contribution_file:
-                result = process_entity(contribution_path, contribution_file)
+                result = process_entity(contribution_path, contribution_file, "zoo")
                 processed.append(contribution_file)
                 if result["status"] == "keep":
                   results.append(result)
         for _, subfile in enumerate(sorted(os.listdir(contribution_path))):
             contribution_file = os.path.join(contribution_path, subfile)
             if ".json" in contribution_file and contribution_file not in processed:
-                result = process_entity(contribution_path, contribution_file)
+                result = process_entity(contribution_path, contribution_file, "photo")
                 processed.append(contribution_file)
                 if result["status"] == "keep":
                   results.append(result)
         all_photos = flatten_comprehension([p["photos"] for p in results])
-        copy_images_to_image_server(all_photos)
-        # TODO: we did all the reviews here, so turn the submission folder into
+        # copy_images_to_image_server(all_photos)
+        # We did all the reviews here, so turn the submission folder into
         # a Git commit on a new redpandafinder branch
+        repo = git.Repo(".")
+        git_new_branch(repo)
+        for result in results:
+            merge = merge_configuration(result)
+            if merge == None:
+                continue
+            if (merge.type != "photo"):
+                repo.index.add(merge.config)   # New panda or zoo file
+            message = '+{locator}: {path}'.format(
+                locator=merge.locator,
+                path=os.path.basename(merge.config)
+            )
+            repo.index.commit(message)
 
-def print_configfile_contents(entity_path, contents):
+def merge_configuration(result):
+    """Make a commit for each metadata file in lexicographic order"""
+    def copy_across_configs(in_data, in_section, in_key, out_data, out_section, out_key):
+        out_data.set(
+            out_section,
+            out_key,
+            in_data.get(in_section, in_key)
+        )
+    in_data = ProperlyDelimitedConfigParser()
+    in_data.read(result["config_path"])
+    if in_data.has_section("panda"):
+        # create new panda id based on reading redpanda.json _totals.pandas
+        # and finding the right zoo folder for zoo ID
+        # TODO: panda form must take in the exact zoo id
+        return {
+            "author": "TODO",
+            "config": "TODO",
+            "locator": "panda",
+            "type": "panda"
+        }
+    elif in_data.has_section("zoo"):
+        # create new zoo id based on reading redpanda.json _totals.zoos
+        # TODO: country form on zoos page, which it tries to guess from address
+        return {
+            "author": "TODO",
+            "config": "TODO",
+            "locator": "zoo",
+            "type": "zoo"
+        }
+    elif in_data.has_section("photo"):
+        # find existing panda or zoo file by search, and incrementally
+        # add photos, or replace existing ig:// locators
+        id_number = in_data.get("photo", "_id")
+        check_id = '{:04d}'.format(abs(id_number))   # Up to three leading zeroes
+        if (id_number > 0):
+            index = PANDA_INDEX
+            section = "panda"
+        else:
+            index = ZOO_INDEX
+            section = "zoo"
+        out_path = index[check_id]
+        out_data = ProperlyDelimitedConfigParser()
+        out_data.read(out_path)
+        out_photo_count = count_until_next_photo(out_data, section)
+        in_photo_count = 1
+        in_photo_key = 'photo.{index}'.format(index=in_photo_count)
+        out_photo_key = 'photo.{index}'.format(index=out_photo_count)
+        copy_across_configs(in_data, "photo", in_photo_key, out_data, section, out_photo_key)
+        in_photo_author = 'photo.{index}.author'.format(index=in_photo_count)
+        out_photo_author = 'photo.{index}.author'.format(index=out_photo_count)
+        copy_across_configs(in_data, "photo", in_photo_author, out_data, section, out_photo_author)
+        in_photo_date = 'photo.{index}.commitdate'.format(index=in_photo_count)
+        out_photo_date = 'photo.{index}.commitdate'.format(index=out_photo_count)
+        copy_across_configs(in_data, "photo", in_photo_date, out_data, section, out_photo_date)
+        in_photo_link = 'photo.{index}.link'.format(index=in_photo_count)
+        out_photo_link = 'photo.{index}.link'.format(index=out_photo_count)
+        copy_across_configs(in_data, "photo", in_photo_link, out_data, section, out_photo_link)
+        in_photo_tags = 'photo.{index}.tags'.format(index=in_photo_count)
+        out_photo_tags = 'photo.{index}.tags'.format(index=out_photo_count)
+        copy_across_configs(in_data, "photo", in_photo_tags, out_data, section, out_photo_tags)
+        with open(out_path, "w") as wfh:
+            out_data.write(wfh)
+        return {
+            "author": in_data.get("photo", in_photo_author),
+            "config": out_path,
+            "locator": in_data.get("photo", in_photo_key),
+            "type": "photo"
+        }
+    else:
+        return None
+
+def print_configfile_contents(config_path):
     """Display the config file path and its contents, with a divider in-between"""
-    config_path = entity_path.replace('.json', '.txt')
     print()
     print(config_path)
     print("-" * len(config_path))
     with open(config_path, "r") as cfh:
         print(cfh.read())
+    # TODO: print notes as a separate field from the entity, if notes exist
 
-def process_entity(contribution_path, entity_path):
+def process_entity(contribution_path, entity_path, entity_type):
     """Show a metadata file converted from json into configparser format, and
     load a carousel of its (resized) images.
     
@@ -219,7 +346,7 @@ def process_entity(contribution_path, entity_path):
         config_path = entity_path.replace(".json", ".txt")
         entity_file = rfh.read()
         convert_json_to_configparser(entity_path, entity_file)
-        print_configfile_contents(entity_path, entity_file)
+        print_configfile_contents(config_path)
         photo_paths = get_image_locators(contribution_path, entity_path, entity_file)
         resize_images(photo_paths)
         xli = display_images(photo_paths)
@@ -227,9 +354,9 @@ def process_entity(contribution_path, entity_path):
         if (decision == "c"):
             xli.kill()
             return {
-                "entity": entity_path,
+                "config": config_path,
                 "photos": photo_paths,
-                "status": "keep",
+                "status": "keep"
             }
         elif (decision == "d"):
             cleanup_list = [entity_path]
@@ -238,18 +365,18 @@ def process_entity(contribution_path, entity_path):
                 os.unlink(file_path)
             xli.kill()
             return {
-                "entity": entity_path,
+                "config": config_path,
                 "photos": photo_paths,
-                "status": "remove",
+                "status": "remove"
             }
         else:
             editor_command = 'vim {config_path}'.format(config_path = config_path)
             os.system(editor_command)
             xli.kill()
             return {
-                "entity": entity_path,
+                "config": config_path,
                 "photos": photo_paths,
-                "status": "keep",
+                "status": "keep"
             }
         
 def prompt_for_decision():
@@ -292,6 +419,7 @@ def resize_images(photo_paths):
             resized.save(photo_path)
 
 if __name__ == '__main__':
+    # TODO: make an index of id to folder path for zoos and animals
     config = read_settings()
     processing_folder = copy_review_data_from_submissions_server(config)
     iterate_through_contributions(processing_folder)
