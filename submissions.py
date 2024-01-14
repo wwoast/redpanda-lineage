622af9bb7a2cb815c0a54f6a50604af89d69e67b
wwoast <boil.afraid@gmail.com>
2024-01-14 10:51:09 -0800

    remove empty tags, if none were added

--- submissions.py
+++ submissions.py
@@ -445,6 +445,7 @@ def merge_configuration(result):
             out_list = out_tags.split(', ')
             tag_list.extend(out_list)
             tag_set = sorted(set(tag_list))
+            tag_set.remove("")
             out_data.set(section, out_photo_tags, ', '.join(tag_set))
         with open(out_path, "w") as wfh:
             out_data.write(wfh)
