da054b69a4da94004df1489b24ea6a21fc024381
wwoast <boil.afraid@gmail.com>
2025-01-09 22:21:06 -0800

    goodbye deebs

--- js/page.js
+++ js/page.js
@@ -325,7 +325,7 @@ Page.home.render = function() {
     // var kingin = Gallery.memorialPhotoCreditsGroup(L.display, "media.7.gin-kin", ["22", "17"], 3);
     // new_content.appendChild(kingin);
     // Current memorials
-    var memorial_ids = [];
+    var memorial_ids = ["1059"];
     if (!Options.data.hideDeadPandas) {
       var departed = Gallery.memorialPhotoCredits(L.display, memorial_ids, 3, Message.memorial);
       new_content.appendChild(departed);
