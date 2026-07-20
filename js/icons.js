6893fbf7cec326606952724fa91bab697c9bb5b8
wwoast <boil.afraid@gmail.com>
2026-07-20 16:40:03 -0700

    testing: forgot to const something

--- js/icons.js
+++ js/icons.js
@@ -102,9 +102,9 @@ const iconSvg = {
  "venus": "<svg  xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 24 24\" fill=\"none\" stroke=\"currentColor\" stroke-width=\"2\" stroke-linecap=\"round\" stroke-linejoin=\"round\"><path d=\"M12 15v7\" /><path d=\"M9 19h6\" /><circle cx=\"12\" cy=\"9\" r=\"6\" /></svg>",
  "volleyball": "<svg  xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 24 24\" fill=\"none\" stroke=\"currentColor\" stroke-width=\"2\" stroke-linecap=\"round\" stroke-linejoin=\"round\"><path d=\"M11 7a16 16 20 0 1 10.98 4.362\" /><path d=\"M12 12a13 13 0 0 1-8.66 5\" /><path d=\"M16.83 13.634a16 16 0 0 1-9.267 7.328\" /><path d=\"M20.66 17A13 13 0 0 0 12 12a13 13 0 0 1 0-10\" /><path d=\"M8.17 15.366a16 16 0 0 1-1.713-11.69\" /><circle cx=\"12\" cy=\"12\" r=\"10\" /></svg>",
  "worm": "<svg  xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 24 24\" fill=\"none\" stroke=\"currentColor\" stroke-width=\"2\" stroke-linecap=\"round\" stroke-linejoin=\"round\"><path d=\"m19 12-1.5 3\" /><path d=\"M19.63 18.81 22 20\" /><path d=\"M6.47 8.23a1.68 1.68 0 0 1 2.44 1.93l-.64 2.08a6.76 6.76 0 0 0 10.16 7.67l.42-.27a1 1 0 1 0-2.73-4.21l-.42.27a1.76 1.76 0 0 1-2.63-1.99l.64-2.08A6.66 6.66 0 0 0 3.94 3.9l-.7.4a1 1 0 1 0 2.55 4.34z\" /></svg>"
-};
+}
 
-iconMap = {
+const iconMap = {
  "🐯🐻": "panda",
  "🌿🍎": "gift",
  "👦👦": "users",
