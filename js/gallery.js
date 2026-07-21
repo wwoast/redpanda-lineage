6512716c1fdd2fe4e36fd0c40a89d75bd41a2410
wwoast <boil.afraid@gmail.com>
2026-07-21 15:40:38 -0700

    gotta update the index value of the class

--- js/gallery.js
+++ js/gallery.js
@@ -236,6 +236,8 @@ export class Carousel {
     this.singlePhotoCredit(photo_info, photo_id, new_index)
     // And the photographer credit's apple points
     this.userApplePoints(photo_info, photo_id, new_index)
+    // Finally, set the carousel's index value
+    this.index = this.new_index.toString()
   }
 
   /** Replace the photographer's credit info for a panda's photo */
