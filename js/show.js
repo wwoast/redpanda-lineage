85c5cbfae975ae0b650180631b2fa7832f071ec8
wwoast <boil.afraid@gmail.com>
2026-08-11 22:34:41 -0700

    filter and then do its own lengths

--- js/show.js
+++ js/show.js
@@ -2657,10 +2657,13 @@ export const resultsPage = {
     const born_link = document.createElement('a')
     born_link.href = `#query/born at ${info._id}`
     const born_at_zoo = Pandas.searchPandaZooBornRecords(info._id, false)
+    const born_known_birthdays = born_at_zoo.filter(panda =>
+      panda.birthday != "unknown")
     const born_count = born_at_zoo.length
+    const born_known_count = born_known_birthdays.length
     if (born_count > 0) {
-      const earliest_born_year = born_at_zoo
-        .filter(panda => panda.birthday != "unknown")[-1]["birthday"].split("/")[0]
+      const earliest_born_year =
+        born_known_birthdays[born_known_count]["birthday"].split("/")[0]
       let output_text = ""
       for (const i in Message.Text.zoo_details_babies[language]) {
         const field = Message.Text.zoo_details_babies[language][i]
