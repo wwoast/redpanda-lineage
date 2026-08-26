3e9b58d94816b720989e3e0d0dbda00366a9b8c6
wwoast <boil.afraid@gmail.com>
2026-08-25 18:01:15 -0700

    ugh this is awful

--- js/pandas.js
+++ js/pandas.js
@@ -1289,7 +1289,7 @@ function sortByNameWithGroups(nodes, photo_list, language) {
       const desired_url = photo_list
         .map(photo => photo.url)
         .filter(url => node.photos.map(photo => photo.url).includes(url))[0]
-      const desired_photo = node.photos.filter(photo => photo.url == desired_url)
+      const desired_photo = node.photos.filter(photo => photo.url == desired_url)[0]
       node.name[language] = groupMediaCaption(node, desired_photo)
     }
     return node
